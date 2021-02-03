bc1fdf9bb516458b03eb31fa6c09986fb4e8e50f
Karson Krieg <klkrieg3@gmail.com>
2021-02-03 13:09:13 -0600

    added 2408 testing

--- src/com/tts/Main.java
+++ src/com/tts/Main.java
@@ -1,9 +1,41 @@
 package com.tts;
 
+import java.util.Arrays;
+
 public class Main {
 
+    public static int SIDE = 4;
+    private static int[][] gameField = new int[SIDE][SIDE];
+
+    private static void rotateClockwise(){
+        int[][] ghostField = new int [SIDE][SIDE];
+        for(int i = 0; i < SIDE; i++){
+            int cur = (SIDE - 1) - i;
+            for(int j = 0; j <SIDE;j ++){
+                ghostField[j][cur] = gameField[i][j];
+//                for (int[] row: ghostField) {
+//                    System.out.println(Arrays.toString(row));
+//               }
+            }
+        }
+        gameField = ghostField;
+    }
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("test");
+
+        for(int i = 0; i < SIDE; i++){
+            for (int j = 0; j < SIDE; j++) {
+                 gameField[i][j]= (int) Math.pow(2, i);
+            }
+        }
+
+        for (int[] row: gameField) {
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println("-------------");
+        rotateClockwise();
+        for (int[] row: gameField) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
